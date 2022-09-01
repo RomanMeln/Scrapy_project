0645147834f7b6983c69a09dd0114becccf75b82
roman <tupozarabotok>
2022-09-01 20:43:20 +0300

    change settings

--- scrapingclub/scrapingclub/settings.py
+++ scrapingclub/scrapingclub/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'scrapingclub.spiders'
 #USER_AGENT = 'scrapingclub (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
